dc11cd82ecd43c144f7ca09fc4a980a1e0a40025
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-25 15:51:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ array=string.split(/ /)
 new_string=""
 array.each do |n|
   n.reverse!
-  new_string << n
+  new_string << n + " "
 end 
-puts new_string + " "
+puts new_string 
 end
 reverse_each_word("Hello, moma")
 
