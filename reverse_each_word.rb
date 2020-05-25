8e8d0d91e89085296cb998eded56815ea6e8a00f
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-25 16:10:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,4 @@
-def reverse_each_word(string)
-array=string.split(/ /)
-new_array=[]
-array.each do |n|
-  n.reverse!
-  new_array << n 
-end 
- new_array.join(" ") 
-end
+
 
 
 def reverse_collect_word(string)
