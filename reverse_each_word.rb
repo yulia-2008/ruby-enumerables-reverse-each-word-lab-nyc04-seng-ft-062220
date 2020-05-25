970a0ac6c0aceb9aee229a1260e79140a73c9764
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-25 15:54:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-array=string.split(/ /)
+array=string.split(/,/)
 new_string=""
 array.each do |n|
   n.reverse!
-  new_string << n + ""
+  new_string << n 
 end 
  new_string 
 end
